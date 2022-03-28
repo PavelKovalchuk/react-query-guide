746f1306c42337c3b470bd64d6a56addff03f2f7
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-03-28 15:17:01 +0300

    mutatation basic

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -57,7 +57,7 @@ export function useUser(): UseUser {
     queryClient.setQueryData(queryKeys.user, null);
 
     // remove user appointments in react query cache
-    queryClient.removeQueries('user-appointments');
+    queryClient.removeQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return { user, updateUser, clearUser };
